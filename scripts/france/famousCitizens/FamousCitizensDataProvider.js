8cc49f6f6b9828cc072303985397a0abcb069b1d
tgann11 <morganne1170@gmail.com>
2021-08-24 12:19:53 -0500

    updated country content

--- scripts/france/famousCitizens/FamousCitizensDataProvider.js
+++ scripts/france/famousCitizens/FamousCitizensDataProvider.js
@@ -1,1 +1,12 @@
-                     
+export const getFamousCitizens = () =>{
+    return famousCitizens
+}
+const famousCitizens = [
+    "Coco Chanel",
+    "Vanessa Paradis",
+    "Catherine Deneuve",
+    "Carla Bruni",
+    "Diane Kruger",
+    "Michael Vartan"
+
+]                        
