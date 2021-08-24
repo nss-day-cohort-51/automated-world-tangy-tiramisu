959fd041bf60428b76ae197f526d1b396f58f348
tgann11 <morganne1170@gmail.com>
2021-08-24 14:24:54 -0500

    updating objects in javascript

--- scripts/france/famousCitizens/FamousCitizensDataProvider.js
+++ scripts/france/famousCitizens/FamousCitizensDataProvider.js
@@ -2,11 +2,41 @@ export const getFamousCitizens = () =>{
     return famousCitizens
 }
 const famousCitizens = [
-    "Coco Chanel",
-    "Vanessa Paradis",
-    "Catherine Deneuve",
-    "Carla Bruni",
-    "Diane Kruger",
-    "Michael Vartan"
+    {
+        name: "Coco Chanel",
+        occupation: "Fashion Designer",
+        description: "Founder and namesake of the infamous Chanel brand, Coco Chanel created an empire and earned herself the title of one of the most influential people of the 20th century.",
+        website: "https://en.wikipedia.org/wiki/Coco_Chanel"
+    },
 
+    {
+        name: "Vanessa Paradis",
+        occupation: "Singer, Model, and Actress",
+        description: "A former child actress, Vanessa Paradis has had a successful career as a multi-faceted artist.",
+        website: "https://en.wikipedia.org/wiki/Vanessa_Paradis"
+    },
+    {
+        name: "Catherine Deneuve",
+        occupation: "Actress, singer, model, Producer",
+        description: "Gaining recognition with portrayal of icy, aloof, and mysterious, Catherine Deneuve became an actress at a young age and went on to become the face of Marianne.",
+        website: "https://en.wikipedia.org/wiki/Catherine_Deneuve"
+    },
+    {
+        name: "Carla Bruni",
+        occupation: "singer-songwriter and model",
+        description: "Starting her career as a model, Carla Bruni took up a career as a songwriter. Bruni has had several chart topping songs, and sold 5 million albums during her career.",
+        website: "https://en.wikipedia.org/wiki/Carla_Bruni"
+    },
+    {
+        name: "Diane Kruger",
+        occupation: "Actress and model",
+        description: "From ballerina, to modeling, to acting, Diane Kruger has done it all with grace. In 2017 she won the award for best actress in the Cannes Film Festival.",
+        website: "https://en.wikipedia.org/wiki/Diane_Kruger"
+    },
+    {
+        name: "Michael Vartan",
+        occupation: "Actor",
+        description: "Drama actor Michael Vartan began his career starring in European films before moving over to America. A small and large screen star.",
+        website: "https://en.wikipedia.org/wiki/Michael_Vartan"
+    },
 ]                        
