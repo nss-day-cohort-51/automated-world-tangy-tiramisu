1a0be46b98f612947f59db657d5f85a962716f34
sudopigeek <samuelcatferrell@gmail.com>
2021-08-24 14:21:20 -0700

    Added main country content

--- scripts/italy/cities/CityDataProvider.js
+++ scripts/italy/cities/CityDataProvider.js
@@ -3,10 +3,40 @@ export const getCities = () => {
 }
 
 const Cities = [
-    "Rome",
-    "Venice",
-    "Milan",
-    "Naples",
-    "Turin",
-    "Palermo"
+    {
+        name: "Rome",
+        population: "4.2 million",
+        description: "Rome (Italian and Latin: Roma [ˈroːma]) is the capital city and a special comune of Italy (named Comune di Roma Capitale), as well as the capital of the Lazio region and has been a major human settlement for almost three millennia.",
+        website: "https://en.wikipedia.org/wiki/Rome"
+    },
+    {
+        name: "Venice",
+        population: "258,600",
+        description: "Venice, Italy is known for being one of the most beautiful and remarkable cities on the planet. It is in northern Italy and is made up of 118 small islands with each one connected by canals and bridges.",
+        website: "https://worldpopulationreview.com/world-cities/venice-population"
+    },
+    {
+        name: "Milan",
+        population: "1.3 million",
+        description: "https://www.britannica.com/place/Milan-Italy",
+        website: "https://www.britannica.com/place/Milan-Italy"
+    },
+    {
+        name: "Naples",
+        population: "2.1 million",
+        description: "The third-largest city in Italy is chock full of history, art, architecture and delicious food, yet it gets a bad rap for its crime and grime. While these blemishes are certainly a reality for residents, they're far from ubiquitous, especially where tourism is concerned.",
+        website: "https://travel.usnews.com/Naples_Italy/"
+    },
+    {
+        name: "Turin",
+        population: "1.7 million",
+        description: "Turin, in Italy, is a former industrial city that was completely redesigned for the 2006 Olympics. It's now one of the most pleasant Italian cities to visit! Just like other more visited Italian cities such as Rome or Venice, Turin clearly deserves to be visited for a weekend or a few more days.",
+        website: "https://en.wikipedia.org/wiki/Turin"
+    },
+    {
+        name: "Palermo",
+        population: "673,735",
+        description: "Palermo is an urban melting pot characterized by history and culture so diverse that it often feels conflicted. Discover an intriguing range of architectural styles, from Arabian domes to baroque buildings, and enjoy delicious cuisine that spans a variety of origins.",
+        website: "https://www.tripadvisor.com/Tourism-g187890-Palermo_Province_of_Palermo_Sicily-Vacations.html"
+    }
 ]
