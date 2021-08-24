7d5b80c62ee22f225fb3951b0d58c0f3f98cc99d
Susie Stanley <susiestanley00@gmail.com>
2021-08-24 15:10:52 -0500

    updated country data

--- scripts/canada/famousCitizens/FamousCitizensDataProvider.js
+++ scripts/canada/famousCitizens/FamousCitizensDataProvider.js
@@ -3,10 +3,43 @@ export const getFamousCitizens = () => {
 }     
      
      const famousCitizens = [
-         "Ryan Reynolds",
-         "Alanis Morissette",
-         "Jim Carrey",
-         "Terry Fox",
-         "Avril Lavigne",
-         "Celine Dion"
-     ]                    
+        {
+            name: "Ryan Reynolds",
+            occupation: "Actor",
+            description: "Ryan Rodney Reynolds is a Canadian movie and television actor who became famous after starring in Buying the Cow and National Lampoon's Van Wilder comedies. He is also known as the star of Green Lantern and Deadpool superhero movies. He was born and raised in Vancouver.",
+            website: "https://www.imdb.com/name/nm0005351/"
+        },   
+        {
+            name: "Alanis Morissette",
+            occupation: "Singer",
+            description: "Alanis Nadine Morissette was born in Ottawa, Canada. Alanis' Greek name is a feminine version of her father's name, Alan. Her mother's name's Georgia and she has two brothers: Chad and Wade (Alanis' Twin). She learned to play the piano at age 6 and the guitar at age 21.",
+            website: "https://alanis.com/"
+        },
+        {
+            name: "Jim Carrey",
+            occupation: "Comedian",
+            description: "As a child, he performed constantly, for anyone who would watch, and even mailed his résumé to The Carol Burnett Show (1967) at age 10. In junior high, he was granted a few precious minutes at the end of each school day to do stand-up routines for his classmates (provided, of course, that he kept a lid on it the rest of the day).",
+            website: "http://www.jimcarreyonline.com/"
+        },
+        {
+            name: "Terry Fox",
+            occupation: "National Hero",
+            description: "Terrance Stanley Fox was a Canadian athlete, humanitarian, and cancer research activist. In 1980, with one leg having been amputated due to cancer, he embarked on an east to west cross-Canada run to raise money and awareness for cancer research. Although the spread of his cancer eventually cost him his life, his efforts resulted in a lasting, worldwide legacy.",
+            website: "https://terryfox.org/terrys-story/"
+        },
+        {
+            name: "Avril Lavigne",
+            occupation: "Singer",
+            description: "A Canadian singer, songwriter and actress. By the age of 15, she had appeared on stage with Shania Twain, and by 16, she had signed a two-album recording contract with Arista Records worth more than $2 million.",
+            website: "https://avrillavigne.com/"
+        },
+        {
+            name: "Celine Dion",
+            occupation: "Singer",
+            description: "Celine  was the 14th child of Thérèse Tanguay and Adhémar Dion. The Dion family was a humble one of modest means, but never lacked for love. The house was jam-packed with children, and Celine’s mom even had to lay her littlest one to sleep in a drawer.",
+            website: "https://www.celinedion.com/"
+        }
+    ]   
+        
+    
+                         
