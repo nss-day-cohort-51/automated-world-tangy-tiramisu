6626855cc6e8de0087da025c247264d25082113b
sudopigeek <samuelcatferrell@gmail.com>
2021-08-25 09:53:41 -0700

    Added image links in italy data collections

--- scripts/italy/cities/CityDataProvider.js
+++ scripts/italy/cities/CityDataProvider.js
@@ -5,36 +5,42 @@ export const getCities = () => {
 const Cities = [
     {
         name: "Rome",
+        image: "rome.png",
         population: "4.2 million",
         description: "Rome is the capital city and a special comune of Italy (named Comune di Roma Capitale), as well as the capital of the Lazio region and has been a major human settlement for almost three millennia.",
         website: "https://en.wikipedia.org/wiki/Rome"
     },
     {
         name: "Venice",
+        image: "venice.png",
         population: "258,600",
         description: "Venice, Italy is known for being one of the most beautiful and remarkable cities on the planet. It is in northern Italy and is made up of 118 small islands with each one connected by canals and bridges.",
         website: "https://worldpopulationreview.com/world-cities/venice-population"
     },
     {
         name: "Milan",
+        image: "milan.png",
         population: "1.3 million",
         description: "https://www.britannica.com/place/Milan-Italy",
         website: "https://www.britannica.com/place/Milan-Italy"
     },
     {
         name: "Naples",
+        image: "naples.png",
         population: "2.1 million",
         description: "The third-largest city in Italy is chock full of history, art, architecture and delicious food, yet it gets a bad rap for its crime and grime. While these blemishes are certainly a reality for residents, they're far from ubiquitous, especially where tourism is concerned.",
         website: "https://travel.usnews.com/Naples_Italy/"
     },
     {
         name: "Turin",
+        image: "turin.png",
         population: "1.7 million",
         description: "Turin, in Italy, is a former industrial city that was completely redesigned for the 2006 Olympics. It's now one of the most pleasant Italian cities to visit! Just like other more visited Italian cities such as Rome or Venice, Turin clearly deserves to be visited for a weekend or a few more days.",
         website: "https://en.wikipedia.org/wiki/Turin"
     },
     {
         name: "Palermo",
+        image: "palermo.png",
         population: "673,735",
         description: "Palermo is an urban melting pot characterized by history and culture so diverse that it often feels conflicted. Discover an intriguing range of architectural styles, from Arabian domes to baroque buildings, and enjoy delicious cuisine that spans a variety of origins.",
         website: "https://www.tripadvisor.com/Tourism-g187890-Palermo_Province_of_Palermo_Sicily-Vacations.html"
--- scripts/italy/landmarks/LandmarkDataProvider.js
+++ scripts/italy/landmarks/LandmarkDataProvider.js
@@ -5,36 +5,42 @@ export const getLandmarks = () => {
 const Landmarks = [
     {
         name: "Colosseum",
+        image: "colosseum.png",
         year: "80 A.D.",
         description: "Colosseum, also called Flavian Amphitheatre, giant amphitheatre built in Rome under the Flavian emperors.",
         website: "https://www.britannica.com/topic/Colosseum"
     },
     {
         name: "Roman Forum",
+        image: "romanforum.png",
         year: "500 B.C.",
         description: "The Roman Forum, known as Forum Romanum in Latin, was a site located at the center of the ancient city of Rome and the location of important religious, political and social activities.",
         website: "https://www.history.com/topics/ancient-rome/roman-forum"
     },
     {
         name: "Piazza Navona",
+        image: "piazza.png",
         year: "1st Century A.D.",
         description: "Piazza Navona is a public open space in Rome, Italy. It is built on the site of the Stadium of Domitian and follows the form of the open space of the stadium.",
         website: ""
     },
     {
         name: "Trevi Fountain",
+        image: "trevi.png",
         year: "19 B.C.",
         description: "The Trevi Fountain is the most beautiful fountain in the world. Whether under daylight or warmly lit up at night, the fountain is never lonely.",
         website: "https://www.rome.net/trevi-fountain"
     },
     {
         name: "The Pantheon",
+        image: "pantheon.png",
         year: "126 A.D.",
         description: "The Pantheon in Rome is a true architectural wonder. Described as the “sphinx of the Campus Martius”—referring to enigmas presented by its appearance and history, and to the location in Rome where it was built—to visit it today is to be almost transported back to the Roman Empire itself.",
         website: "https://smarthistory.org/the-pantheon/"
     },
     {
         name: "St. Peter's Basilica",
+        image: "stpeters.png",
         year: "329 A.D.",
         description: "St. Peter’s Basilica, also called New St. Peter’s Basilica, present basilica of St. Peter in Vatican City (an enclave in Rome), begun by Pope Julius II in 1506 and completed in 1615 under Paul V.",
         website: "https://www.britannica.com/topic/Saint-Peters-Basilica"
