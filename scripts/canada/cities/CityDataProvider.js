c17c570ff9e2cbff8cd4e9b4c8b0c26a2f6758b2
Susie Stanley <susiestanley00@gmail.com>
2021-08-24 21:38:06 -0500

    updated js data files and uploaded pics to images/canada

--- scripts/canada/cities/CityDataProvider.js
+++ scripts/canada/cities/CityDataProvider.js
@@ -4,39 +4,45 @@ export const getCities = () => {
      
      const Cities = [
         {
+            image: "./images/canada/vancouver.png"
             name: "Vancouver",
-            population: 1886,
+            population: "2.6M",
             description: "Vancouver is consistently named as one of the top five worldwide cities for livability and quality of life, and acknowledged as the first city ranked among the top ten of the world's most well-living cities[13] for ten consecutive years. However, Vancouver also ranks as the most expensive city to live in Canada and as the fourth-most expensive housing market globally. In 2011, the city planned to become the greenest city in the world by 2020. Vancouverism is the city's urban planning design philosophy.",
             website: "https://vancouver.ca/"
         },
         {
+            image: "./images/canada/toronto.png",
             name: "Toronto",
-            population: 1834,
-            desciption: "The city is home to the Toronto Stock Exchange, the headquarters of Canada's five largest banks, and the headquarters of many large Canadian and multinational corporations. Its economy is highly diversified with strengths in technology, design, financial services, life sciences, education, arts, fashion, aerospace, environmental innovation, food services, and tourism.",
+            population: "2.7M",
+            description: "The city is home to the Toronto Stock Exchange, the headquarters of Canada's five largest banks, and the headquarters of many large Canadian and multinational corporations. Its economy is highly diversified with strengths in technology, design, financial services, life sciences, education, arts, fashion, aerospace, environmental innovation, food services, and tourism.",
             website: "https://www.toronto.ca/"
         },
         {
+            image: "./images/canada/ottawa.png",
             name: "Ottawa",
-            population: 22212,
-            desciption: "The city has evolved into the political centre of Canada. Its original boundaries were expanded through numerous annexations. The city name Ottawa was chosen in reference to the Ottawa River, the name of which is derived from the Algonquin Odawa, meaning 'to trade'.",
+            population: "934K",
+            description: "The city has evolved into the political centre of Canada. Its original boundaries were expanded through numerous annexations. The city name Ottawa was chosen in reference to the Ottawa River, the name of which is derived from the Algonquin Odawa, meaning 'to trade'.",
             website: "https://ottawa.ca/en"
         },
         {
+            image: "./images/canada/calgary.png",
             name: "Calgary",
-            population: 22212,
-            desciption: "A detachment of NWMP officers builds Fort Brisebois at the confluence of the Bow and Elbow rivers, in order to route the region’s whiskey traders. Lieutenant-Colonel Macleod renamed it Fort Calgary the following year after Calgary House in his native Scotland. That’s why the city is called Calgary and the north-south corridor is called Macleod Trail.",
+            population: "1.2M",
+            description: "A detachment of NWMP officers builds Fort Brisebois at the confluence of the Bow and Elbow rivers, in order to route the region’s whiskey traders. Lieutenant-Colonel Macleod renamed it Fort Calgary the following year after Calgary House in his native Scotland. That’s why the city is called Calgary and the north-south corridor is called Macleod Trail.",
             website: "https://www.calgary.ca/home.html"
         },
         {
+            image: "./images/canada/montreal.png",
             name: "Montreal",
-            population: 22212,
-            desciption: "The site of Montreal was called Hochelaga by the Huron people when the French navigator and explorer Jacques Cartier visited it on his second voyage to the New World. More than 1,000 Native Americans (some sources say as many as 3,500) welcomed him on the slope of the mountain that he named Mont Royal (Mont Réal).",
+            population: "1.7M",
+            description: "The site of Montreal was called Hochelaga by the Huron people when the French navigator and explorer Jacques Cartier visited it on his second voyage to the New World. More than 1,000 Native Americans (some sources say as many as 3,500) welcomed him on the slope of the mountain that he named Mont Royal (Mont Réal).",
             website: "https://montreal.ca/en/"
         },
         {
+            image: "./images/canada/saskatoon.png",
             name: "Saskatoon",
-            population: 22212,
-            desciption: "Saskatoon is the largest city in the Canadian province of Saskatchewan. It straddles a bend in the South Saskatchewan River in the central region of the province. It is located along the Trans-Canada Yellowhead Highway, and has served as the cultural and economic hub of central Saskatchewan since its founding as a Temperance colony",
+            population: "269K",
+            description: "Saskatoon is the largest city in the Canadian province of Saskatchewan. It straddles a bend in the South Saskatchewan River in the central region of the province. It is located along the Trans-Canada Yellowhead Highway, and has served as the cultural and economic hub of central Saskatchewan since its founding as a Temperance colony",
             website: "https://www.saskatoon.ca/"
         }
      ]           
--- scripts/canada/famousCitizens/FamousCitizensDataProvider.js
+++ scripts/canada/famousCitizens/FamousCitizensDataProvider.js
@@ -4,39 +4,45 @@ export const getFamousCitizens = () => {
      
      const famousCitizens = [
         {
+            image: "./images/canada/ryan.png"
             name: "Ryan Reynolds",
             occupation: "Actor",
-            description: "Ryan Rodney Reynolds is a Canadian movie and television actor who became famous after starring in Buying the Cow and National Lampoon's Van Wilder comedies. He is also known as the star of Green Lantern and Deadpool superhero movies. He was born and raised in Vancouver.",
-            website: "https://www.imdb.com/name/nm0005351/"
+            description: "Ryan Rodney Reynolds is a Canadian actor, comedian, and film producer. He began his career starring in the Canadian teen soap opera Hillside, and had minor roles before landing the lead role on the sitcom Two Guys and a Girl",
+            website: "https://ryan-reynolds.net/"
         },   
         {
+            image: "./images/canada/alanis.png"
             name: "Alanis Morissette",
             occupation: "Singer",
-            description: "Alanis Nadine Morissette was born in Ottawa, Canada. Alanis' Greek name is a feminine version of her father's name, Alan. Her mother's name's Georgia and she has two brothers: Chad and Wade (Alanis' Twin). She learned to play the piano at age 6 and the guitar at age 21.",
+            description: "Alanis Morissette was born in Ottawa, Canada. Her Greek first name is a feminine version of her father's name. She is a Canadian singer, songwriter, musician, and actress. Known for her emotive mezzo-soprano voice, Morissette began her career in Canada in the early 90s",
             website: "https://alanis.com/"
         },
         {
+            image: "./images/canada/jim.png"
             name: "Jim Carrey",
             occupation: "Comedian",
-            description: "As a child, he performed constantly, for anyone who would watch, and even mailed his résumé to The Carol Burnett Show (1967) at age 10. In junior high, he was granted a few precious minutes at the end of each school day to do stand-up routines for his classmates (provided, of course, that he kept a lid on it the rest of the day).",
+            description: "As a child, he performed constantly, for anyone who would watch, &amp; even mailed his résumé to The Carol Burnett Show at 10. In junior high, he was granted a few precious minutes at the end of each day to do stand-up routines for his classmates (provided that he kept a lid on it the rest of the day).",
             website: "http://www.jimcarreyonline.com/"
         },
         {
+            image: "./images/canada/terry.png"
             name: "Terry Fox",
             occupation: "National Hero",
-            description: "Terrance Stanley Fox was a Canadian athlete, humanitarian, and cancer research activist. In 1980, with one leg having been amputated due to cancer, he embarked on an east to west cross-Canada run to raise money and awareness for cancer research. Although the spread of his cancer eventually cost him his life, his efforts resulted in a lasting, worldwide legacy.",
+            description: "Terry was a Canadian athlete, humanitarian, and cancer research activist. In 1980, with one leg having been amputated due to cancer, he embarked on an east to west cross-Canada run to raise money and awareness for cancer research. Although the spread of his cancer eventually cost him his life, his efforts resulted in a lasting, worldwide legacy.",
             website: "https://terryfox.org/terrys-story/"
         },
         {
+            image: "./images/canada/avril.png"
             name: "Avril Lavigne",
             occupation: "Singer",
             description: "A Canadian singer, songwriter and actress. By the age of 15, she had appeared on stage with Shania Twain, and by 16, she had signed a two-album recording contract with Arista Records worth more than $2 million.",
             website: "https://avrillavigne.com/"
         },
         {
+            image: "./images/canada/celine.png"
             name: "Celine Dion",
             occupation: "Singer",
-            description: "Celine  was the 14th child of Thérèse Tanguay and Adhémar Dion. The Dion family was a humble one of modest means, but never lacked for love. The house was jam-packed with children, and Celine’s mom even had to lay her littlest one to sleep in a drawer.",
+            description: "Céline is a Canadian singer. She is noted for her powerful and technically skilled vocals. Dion is the best-selling Canadian recording artist in history. Born into a large family from Quebec, she emerged as a teen star in her home country with a series of French-language albums during the 80s",
             website: "https://www.celinedion.com/"
         }
     ]   
