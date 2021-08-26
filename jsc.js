10851b68cf75e583dce3ee2fa2698b5be05c95b4
sudopigeek <samuelcatferrell@gmail.com>
2021-08-26 13:43:38 -0700

    Finalized javascript

--- jsc.js
+++ jsc.js
@@ -4,47 +4,25 @@ import { ItalyData } from "./scripts/italy/italyList.js";
 import { FranceData } from "./scripts/france/franceList.js";
 import { CanadaData } from "./scripts/canada/canadaList.js";
 import { CountryContainer } from "./containers.js"
-
 enableLink("home")
 renderHomePageCountries()
-
+onCountryClick()
+let current = "Home"
 document.getElementById("home").addEventListener('click', function() {
     enableLink("home")
+    current = "Home"
     renderHomePageCountries()
+    onCountryClick()
 })
-
 document.getElementById("italy").addEventListener('click', function() {
-    enableLink("italy")
-    const container = document.getElementById("Container")
-    container.style = "flex-direction: column;"
-    container.innerHTML = CountryContainer(getCountries()[0], "italy")
-    ItalyData()
+    Render("italy")
 })
-
-document.getElementById("country_italy").addEventListener('click', function() {
-    enableLink("italy")
-    const container = document.getElementById("Container")
-    container.style = "flex-direction: column;"
-    container.innerHTML = CountryContainer(getCountries()[0], "italy")
-    ItalyData()
-})
-
 document.getElementById("france").addEventListener('click', function() {
-    enableLink("france")
-    const container = document.getElementById("Container")
-    container.style = "flex-direction: column;"
-    container.innerHTML = CountryContainer(getCountries()[1], "france")
-    FranceData()
+    Render("france")
 })
-
 document.getElementById("canada").addEventListener('click', function() {
-    enableLink("canada")
-    const container = document.getElementById("Container")
-    container.style = "flex-direction: column;"
-    container.innerHTML = CountryContainer(getCountries()[2], "canada")
-    CanadaData()
+    Render("canada")
 })
-
 function enableLink(linkId) {
     let children = document.getElementById("linkList").children
     for (let i = 0; i < children.length; i++) {
@@ -66,6 +44,50 @@ function renderHomePageCountries() {
     countryElement.innerHTML += countryCard(countries[1], "france")
     countryElement.innerHTML += countryCard(countries[2], "canada")
 }
-function onCountryClick(event) {
-    alert(event.target.id)
+function onCountryClick() {
+    document.getElementById("country_italy").addEventListener('click', function() {
+        Render("italy")
+    })
+    document.getElementById("country_france").addEventListener('click', function() {
+        Render("france")
+    })
+    document.getElementById("country_canada").addEventListener('click', function() {
+        Render("canada")
+    })
 }
+function Render(country) {
+    switch (country) {
+        case "italy":
+            enableLink("italy")
+            current = "Italy"
+            const container = document.getElementById("Container")
+            container.style = "flex-direction: column;"
+            container.innerHTML = CountryContainer(getCountries()[0], "italy")
+            ItalyData() 
+            break;
+        case "france":
+            enableLink("france")
+            current = "France"
+            const container2 = document.getElementById("Container")
+            container2.style = "flex-direction: column;"
+            container2.innerHTML = CountryContainer(getCountries()[1], "france")
+            FranceData()
+            break;
+        case "canada":
+            enableLink("canada")
+            current = "Canada"
+            const container3 = document.getElementById("Container")
+            container3.style = "flex-direction: column;"
+            container3.innerHTML = CountryContainer(getCountries()[2], "canada")
+            CanadaData()
+            break;     
+    }
+}
+document.getElementById("linkList").addEventListener("mouseover", function(event) {
+    event.target.style.color = "#58C2F1"
+})
+document.getElementById("linkList").addEventListener("mouseout", function(event) {
+    if (event.target.innerHTML !== current) {
+        event.target.style.color = "white"
+    }
+})
