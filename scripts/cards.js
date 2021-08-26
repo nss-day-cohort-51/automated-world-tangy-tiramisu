950e95e8175b02d6d167abce1f5306809a98c382
sudopigeek <samuelcatferrell@gmail.com>
2021-08-26 09:31:14 -0700

    Fixed image links

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,7 +1,7 @@
 export const cityCard = (cityObj, country) => {
     return `
         <article class="city-card">
-            <img src="../images/${country}/cities/${cityObj.image}" alt="City Image" />
+            <img src="images/${country}/cities/${cityObj.image}" alt="City Image" />
             <h3>${cityObj.name}</h3>
             <h5>Population: ${cityObj.population}</h5>
             <p>${cityObj.description}</p>
@@ -11,7 +11,7 @@ export const cityCard = (cityObj, country) => {
 export const citizenCard = (citizenObj, country) => {
     return `
         <article class="citizen-card">
-            <img src="../images/${country}/citizens/${citizenObj.image}" alt="Citizen Image" />
+            <img src="images/${country}/citizens/${citizenObj.image}" alt="Citizen Image" />
             <h3>${citizenObj.name}</h3>
             <h5>${citizenObj.occupation}</h5>
             <p>${citizenObj.description}</p>
@@ -21,7 +21,7 @@ export const citizenCard = (citizenObj, country) => {
 export const landmarkCard = (landmarkObj, country) => {
     return `
         <article class="landmark-card">
-            <img src="../images/${country}/landmarks/${landmarkObj.image}" alt="Landmark Image" />
+            <img src="images/${country}/landmarks/${landmarkObj.image}" alt="Landmark Image" />
             <h3>${landmarkObj.name}</h3>
             <h5>Established: ${landmarkObj.year}</h5>
             <p>${landmarkObj.description}</p>
@@ -31,7 +31,7 @@ export const landmarkCard = (landmarkObj, country) => {
 export const countryCard = (countryObj, country) => {
     return `<a href="${country}.html">
             <article class="country-card">
-                <img src="../images/${country}/${countryObj.image}" alt="Country Image" />
+                <img src="images/${country}/${countryObj.image}" alt="Country Image" />
                 <h3>${countryObj.name}</h3>
                 <p>${countryObj.description}</p>
             </article>
